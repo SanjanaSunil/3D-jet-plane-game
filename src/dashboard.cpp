2bea8460fccfdd1123efb8d2097688d4a77f406e
Sanjana Sunil <sanjana99sunil@gmail.com>
2019-02-17 19:09:14 +0530

    added lines of altittude bar

--- src/dashboard.cpp
+++ src/dashboard.cpp
@@ -17,15 +17,54 @@ Dashboard::Dashboard(float x, float y, float z) {
 	};
     this->fuel_bar = create3DObject(GL_TRIANGLES, 6, fuel_vertex_buffer_data, COLOR_GREEN, GL_FILL);
 
-	GLfloat altitude_vertex_buffer_data[] = {
-		-3.8f, 3.1f, 0.0f,
-		-3.7f, 3.1f, 0.0f,
-		-3.7f,-3.1f, 0.0f,
-		-3.8f, 3.1f, 0.0f,
-		-3.7f,-3.1f, 0.0f,
-		-3.8f,-3.1f, 0.0f
-	};
-	this->altitude_bar = create3DObject(GL_TRIANGLES, 6, altitude_vertex_buffer_data, COLOR_BLACK, GL_FILL);
+	float height = 6.2f;
+	int bar_no = 10;
+	GLfloat altitude_vertex_buffer_data[18+(bar_no+1)*18];
+	for(int i=0; i<18; ++i) // Backbone
+	{
+		if((i+1)%3==0) altitude_vertex_buffer_data[i] = 0.0f;
+
+		if((i+1)%3==2) 
+		{
+			altitude_vertex_buffer_data[i] = 3.1f;
+			if(i==7 || i==13 || i==16) altitude_vertex_buffer_data[i] *= -1;
+		}
+
+		if((i+1)%3==1) 
+		{
+			altitude_vertex_buffer_data[i] = -3.7f;
+			if(i==3 || i==6 || i==12) altitude_vertex_buffer_data[i] = -3.65f;
+		}
+	}
+	float start_y = -3.1f;
+	int i = 18;
+	for(int count=0; count<=bar_no; ++count) 
+	{
+		altitude_vertex_buffer_data[i++] = -3.8f;
+		altitude_vertex_buffer_data[i++] = start_y + count*(float)height/bar_no;
+		altitude_vertex_buffer_data[i++] = 0.0f;
+
+		altitude_vertex_buffer_data[i++] = -3.55f;
+		altitude_vertex_buffer_data[i++] = start_y + count*(float)height/bar_no;
+		altitude_vertex_buffer_data[i++] = 0.0f;
+
+		altitude_vertex_buffer_data[i++] = -3.55f;
+		altitude_vertex_buffer_data[i++] = start_y + count*(float)height/bar_no - 0.02f;
+		altitude_vertex_buffer_data[i++] = 0.0f;
+
+		altitude_vertex_buffer_data[i++] = -3.8f;
+		altitude_vertex_buffer_data[i++] = start_y + count*(float)height/bar_no;
+		altitude_vertex_buffer_data[i++] = 0.0f;
+
+		altitude_vertex_buffer_data[i++] = -3.8f;
+		altitude_vertex_buffer_data[i++] = start_y + count*(float)height/bar_no - 0.02f;
+		altitude_vertex_buffer_data[i++] = 0.0f;
+
+		altitude_vertex_buffer_data[i++] = -3.55f;
+		altitude_vertex_buffer_data[i++] = start_y + count*(float)height/bar_no - 0.02f;
+		altitude_vertex_buffer_data[i++] = 0.0f;
+	}
+	this->altitude_bar = create3DObject(GL_TRIANGLES, 6+(bar_no+1)*6, altitude_vertex_buffer_data, COLOR_BLACK, GL_FILL);
 }
 
 void Dashboard::draw(glm::mat4 VP) {
