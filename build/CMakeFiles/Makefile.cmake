3d546a1444d6c5b223a829648ca5b1c2744419ac
Sanjana Sunil <sanjana99sunil@gmail.com>
2019-02-20 13:00:25 +0530

    renamed executable

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -121,5 +121,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/graphics_asgn1.dir/DependInfo.cmake"
+  "CMakeFiles/game.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/bomb.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/bomb.cpp.o"
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/checkpoint.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/checkpoint.cpp.o"
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/color.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/color.cpp.o"
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/dashboard.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/dashboard.cpp.o"
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/fuelup.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/fuelup.cpp.o"
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/input.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/input.cpp.o"
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/island.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/island.cpp.o"
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/main.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/main.cpp.o"
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/missile.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/missile.cpp.o"
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/nonedit.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/nonedit.cpp.o"
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/other_handlers.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/other_handlers.cpp.o"
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/parachute.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/parachute.cpp.o"
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/plane.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/plane.cpp.o"
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/ring.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/ring.cpp.o"
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/sea.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/sea.cpp.o"
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/target.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/target.cpp.o"
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/timer.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/timer.cpp.o"
+  "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/src/volcano.cpp" "/home/sanjanasunil/subjects/graphics/assignments/2-assign/jet-plane-3d-game/build/CMakeFiles/game.dir/src/volcano.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libdrm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
